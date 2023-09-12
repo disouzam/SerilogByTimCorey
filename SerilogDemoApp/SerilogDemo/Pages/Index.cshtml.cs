8d2fc3740f57b9c6b0bf11257a00c74d7336dfad
Dickson Souza <disouzam@yahoo.com.br>
2023-09-12 16:11:22 -0300

    Added a try..catch block to simulate different types of logs

--- SerilogDemoApp/SerilogDemo/Pages/Index.cshtml.cs
+++ SerilogDemoApp/SerilogDemo/Pages/Index.cshtml.cs
@@ -14,6 +14,25 @@ namespace SerilogDemo.Pages
         public void OnGet()
         {
             _logger.LogInformation("You requested the Index page.");
+
+            try
+            {
+                for(int i = 0; i < 100; i++)
+                {
+                    if(i == 56)
+                    {
+                        throw new Exception("This is our demo exception.");
+                    }
+                    else
+                    {
+                        _logger.LogInformation("The value of i is {iVariable}", i);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "We caught this exception in the Index Get call.");
+            }
         }
     }
 }
