33e0a552c3e3ea29d51810ba2487113922f2d9d9
Dickson Souza <disouzam@yahoo.com.br>
2023-09-12 22:47:48 -0300

    Configuration from Serilog almost completed, with some subtle differences due some methods being made obsolete compared to the ones used by Tim Corey in his video and also due to not being using Startup.cs in .NET 6.0

--- SerilogDemoApp/SerilogDemo/Program.cs
+++ SerilogDemoApp/SerilogDemo/Program.cs
@@ -22,31 +22,48 @@ namespace SerilogDemo
                 .ReadFrom.Configuration(configuration)
                 .CreateLogger();
 
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddRazorPages();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if(!app.Environment.IsDevelopment())
+            try
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                Log.Information("Application Starting up...");
+
+                var builder = WebApplication.CreateBuilder(args);
+
+                builder.Host.UseSerilog();
+
+                // Add services to the container.
+                builder.Services.AddRazorPages();
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if(!app.Environment.IsDevelopment())
+                {
+                    app.UseExceptionHandler("/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
+
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
+
+                app.UseSerilogRequestLogging();
+
+                app.UseRouting();
+
+                app.UseAuthorization();
+
+                app.MapRazorPages();
+
+                app.Run();
             }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapRazorPages();
-
-            app.Run();
+            catch(Exception ex)
+            {
+                Log.Fatal(ex, "The application failed to start correctly!");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
